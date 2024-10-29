766eebd558eaa6664e67389f08122a068698bd54
Addisyn <spilleraddisyn@gmail.com>
2024-10-29 23:17:34 +0000

    This is the code that finds the factorial of a number

---
+++ .codesandbox/main.py
@@ -0,0 +1,8 @@
+#This imports the math library
+import math
+#This is the function that will find the factorial
+def factorial(n):
+    return math.factorial(n)
+
+#This calls the factorial function
+print(factorial(5))
